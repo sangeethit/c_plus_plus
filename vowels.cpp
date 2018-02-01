606a069fc3a982e0e9bf7a880245d34885a1240c
sangeethit <34642732+sangeethit@users.noreply.github.com>
2018-02-01 13:18:35 +0530

    Create vowels.cpp

---
+++ vowels.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char x;
+    cin>>x;
+    if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u'||x=='A'||x=='E'||x=='I'||x=='O'||x=='U')
+     cout<<"vowels";
+    else
+     cout<<"not a vowels";
+    return 0;
+}
